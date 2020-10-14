1603e19f2e0e8504e39a387506165b8f52aad479
Ian-c-cameron <ian.c.cam@gmail.com>
2020-10-14 21:25:24 +0000

    create database and initial tables

---
+++ migrations/01_schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS lightbnb;
+CREATE DATABASE lightbnb;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  active BOOLEAN,
+  title VARCHAR(255),
+  description TEXT,
+  cost_per_night BIGINT, --in cents
+  parking_spaces INTEGER,
+  bathrooms INTEGER,
+  bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  thumbnail TEXT,
+  cover_photo TEXT
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) on DELETE CASCADE,
+  message TEXT,
+  rating INTEGER
+);
