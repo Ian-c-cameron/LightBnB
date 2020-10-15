e1806b6313481bd596281d30e04e6177a453c24a
Ian-c-cameron <ian.c.cam@gmail.com>
2020-10-15 03:55:37 +0000

    connect to DB

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+//Database setup
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 /// Users
 
 /**
